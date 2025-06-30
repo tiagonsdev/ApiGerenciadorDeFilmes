8bd5ec239882a8d48cca068708bce6f54a338e81
Tiago Novaes - Ariki Empresas <tiago.novaes-ae@Ariki.Local>
2025-06-30 08:00:40 -0300

    first commit

---
+++ src/main/java/com/Senac/GerenciadorDeFilmes/GerenciadorDeFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.Senac.GerenciadorDeFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Senac/GerenciadorDeFilmes/GerenciadorDeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Senac.GerenciadorDeFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
