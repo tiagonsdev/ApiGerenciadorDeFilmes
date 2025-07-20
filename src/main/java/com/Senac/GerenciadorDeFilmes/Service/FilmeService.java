6ad6299c59355a40236691a15c6d96968fd630ff
Tiago Novaes <tiagonsousadev@gmail.com>
2025-07-20 15:22:54 -0300

    criada a estrutura da classe service

--- src/main/java/com/Senac/GerenciadorDeFilmes/Service/FilmeService.java
+++ src/main/java/com/Senac/GerenciadorDeFilmes/Service/FilmeService.java
@@ -5,13 +5,28 @@ import com.Senac.GerenciadorDeFilmes.Repository.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FilmeService {
 
     @Autowired
     private FilmeRepository filmeRepository;
 
-    public FilmeModel cadastrarFilme(FilmeModel filmeModel){
+    public FilmeModel salvar(FilmeModel filmeModel){
         return filmeRepository.save(filmeModel);
+    }
+
+    public List<FilmeModel> listarTodos(){
+        return filmeRepository.findAll();
+    }
+
+    public Optional<FilmeModel> buscarPorId(Long id){
+        return filmeRepository.findById(id);
+    }
+
+    public void deletar(Long id){
+         filmeRepository.deleteById(id);
     }
 }
