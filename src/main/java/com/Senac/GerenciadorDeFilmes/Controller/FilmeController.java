eaf350a880b66dea19fc9b1af3a8cac94ca49871
Tiago Novaes <tiagonsousadev@gmail.com>
2025-07-20 15:49:47 -0300

    controller criado com exito e mais algumas outras atualizações

--- src/main/java/com/Senac/GerenciadorDeFilmes/Controller/FilmeController.java
+++ src/main/java/com/Senac/GerenciadorDeFilmes/Controller/FilmeController.java
@@ -3,26 +3,57 @@ package com.Senac.GerenciadorDeFilmes.Controller;
 import com.Senac.GerenciadorDeFilmes.Entity.FilmeModel;
 import com.Senac.GerenciadorDeFilmes.Service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/filmes")
 public class FilmeController {
 
     @Autowired
     private FilmeService filmeService;
 
-    @GetMapping("/form")
-    public String exibirFormulario(Model model) {
-        model.addAttribute("filme", new FilmeModel());
-        return "formulario"; // procura templates/formulario.html
+    @GetMapping
+    public List<FilmeModel> listar() {
+        return filmeService.listarTodos();
     }
 
-    @PostMapping("/salvar")
-    public String salvarFilme(@ModelAttribute FilmeModel filme) {
-        filmeService.cadastrarFilme(filme);
-        return "redirect:/filmes/form?sucesso";
+    @GetMapping("/id")
+    public ResponseEntity<FilmeModel> buscar(@PathVariable Long id) {
+        return filmeService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    public ResponseEntity<FilmeModel> criar(@RequestBody FilmeModel filme) {
+
+        FilmeModel salvo = filmeService.salvar(filme);
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<FilmeModel> atualizar(@PathVariable Long id, @RequestBody FilmeModel dados) {
+        return filmeService.buscarPorId(id).map(filme -> {
+            filme.setFilme(dados.getFilme());
+            filme.setAnalise(dados.getAnalise());
+            return ResponseEntity.ok(filmeService.salvar(filme));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<Void> deletar(@PathVariable Long id){
+        if (filmeService.buscarPorId(id).isPresent()){
+            filmeService.deletar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
+
